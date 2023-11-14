003c16496329a98c9bdba391cd81094a210358cd
diyorbekjoraqulov8 <diyorbekjoraqulov8@gmail.com>
2023-11-14 10:07:53 +0300

    new 2

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let addressAll = [];
 const axios = require('axios')
 
 // const token = process.env.TOKEN;
-const token = "6503495559:AAGJGEcF3ngQV5saHEtEsEeClUOJHsf_ZQc";
+const token = "6917279146:AAGrU8I0csJfRlUvX_hpI0swyOOIVpvtFP0";
 
 const bot = new TelegramBot(token, { polling: true });
 
-// bot.setWebHook('https://mahallabot.onrender.com')
+bot.setWebHook('https://mahallabot.onrender.com')
 
 let currentAddress;
 let qishloqlar;
